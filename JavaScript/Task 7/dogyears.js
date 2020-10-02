74fbef5ce8aeb57ec20cd6dad191d1ed92d5cbcc
vid2021 <44609123+vid2021@users.noreply.github.com>
2020-10-02 15:26:43 +0530

    Create dogyears.js

---
+++ JavaScript/Task 7/dogyears.js
@@ -0,0 +1,13 @@
+var myAge=20;                                    //myAge contains age
+var earlyYears = 2;                             //value of earlyYears can change
+earlyYears = earlyYears*10.5;
+var laterYears = myAge-2;                      //since we took care of 2 years we are subtracting 2 from myAge
+laterYears = laterYears*4;                    //after 2 years each year is equal to 4 dog years
+myAgeInDogYears = earlyYears+laterYears;
+var myName = "Vidhi";          
+myName = myName.toLowerCase();               //convering myName into lowercase
+console.log("My name is "+myName+". I am "+myAge+" years old in human years which is "+myAgeInDogYears+" years old in dog years."); // Displaying myName, myAge in human yeras and in dogyears
+
+
+
+
