7c0c30f09749dcc8e01995130e7617fc3aecf2d6
vid2021 <44609123+vid2021@users.noreply.github.com>
2020-10-06 23:56:42 +0530

    Add files via upload

---
+++ React/Task 23/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import BusinessList from "./components/BusinessList/BusinessList.js";
+import SearchBar from "./components/SearchBar/SearchBar.js";
+
+class App extends React.Component() {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
