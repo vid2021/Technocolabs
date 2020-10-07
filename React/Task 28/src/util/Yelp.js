39bd497c0559c68dbb2ec25abf12641361d0a315
vid2021 <44609123+vid2021@users.noreply.github.com>
2020-10-07 15:33:30 +0530

    Add files via upload

---
+++ React/Task 28/src/util/Yelp.js
@@ -0,0 +1,36 @@
+const apiKey =
+  "_J_7OsORmhiKx62NdameQT3lgIxAgoAAoefazuKP3vS6n391Bnb6XoXc-V0eSQmJ0YYzbj6Zsy20Ve5J0nYbX1RL5dtkuA2TVEdyj6yVq7Is51VE2qSQ_xwKsIF9X3Yx";
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map((business) => ({
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.state,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].title,
+            rating: business.rating,
+            reviewCount: business.review_count,
+          }));
+        }
+      });
+  },
+};
+
+export default Yelp;
